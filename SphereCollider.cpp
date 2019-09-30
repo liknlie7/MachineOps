51df44069ee35272303668caf8cf820e99142bf6
liknlie7 <liknlie@gmail.com>
2019-10-01 01:52:01 +0900

    SphereColliderの追加

---
+++ SphereCollider.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "SphereCollider.h"
+
+
+SphereCollider::SphereCollider()
+{
+}
+
+
+SphereCollider::~SphereCollider()
+{
+}
---
+++ SphereCollider.h
@@ -0,0 +1,8 @@
+#pragma once
+class SphereCollider
+{
+public:
+	SphereCollider();
+	~SphereCollider();
+};
+
