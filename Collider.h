d09f62a0393af9da3db712272049770fe8fd3d9e
liknlie7 <liknlie@gmail.com>
2019-10-01 04:15:06 +0900

    Collider機能追加

--- Collider.h
+++ Collider.h
@@ -3,6 +3,8 @@
 
 #include <SimpleMath.h>
 
+#include "GameObject.h"
+#include "Collider.h"
 
 class GameObject;
 class SphereCollider;
@@ -11,29 +13,46 @@ class BoxCollider;
 
 class Collider
 {
-protected:
-	GameObject*                  m_owner;
-	DirectX::SimpleMath::Vector3 m_offset;
+public: // 基本
 
-
-public:
+	// コンストラクタ
 	Collider(GameObject* owner);
-
-public:
+	// デストラクタ
 	virtual ~Collider() = default;
 
+public: // アクセサ
 
-public:
+	// 座標取得
+	const DirectX::SimpleMath::Vector3 Collider::GetPosition() const
+	{
+		return m_owner->GetPosition() + m_offset;
+	}
+
+	// 補正値の取得
+	const DirectX::SimpleMath::Vector3 Collider::GetOffset() const
+	{
+		return m_offset;
+	}
+
+	// 補正値の設定
+	void Collider::SetOffset(const DirectX::SimpleMath::Vector3 & offset)
+	{
+		m_offset = offset;
+	}
+
+public: // 関数
+
 	virtual bool IsCollided(const Collider*       collider) const = 0;
 	virtual bool IsCollided(const SphereCollider* collider) const;
 	virtual bool IsCollided(const BoxCollider*    collider) const;
 
-
-public:
 	void OnCollision(const Collider* object) const;
 
-	const DirectX::SimpleMath::Vector3 GetPosition() const;
-	const DirectX::SimpleMath::Vector3 GetOffset() const;
+protected: // 変数
 
-	void  SetOffset(const DirectX::SimpleMath::Vector3& offset);
+	// 持ち主
+	GameObject*                  m_owner;
+	// 補正値
+	DirectX::SimpleMath::Vector3 m_offset;
+
 };
